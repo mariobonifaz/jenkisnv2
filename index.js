1f0469585b6a7b9c9e168ff714a246efc89522d0
Mario Viloria <211110@ids.upchiapas.edu.mx>
2024-06-05 23:37:28 -0600

    debugger{jenkins pipeline try2}

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('api/v1/welcome', (req,res) => {
-    res.send('hello world!!!!!');
+    res.send('hello world!!!');
 });
 
 app.listen(port, () => {
