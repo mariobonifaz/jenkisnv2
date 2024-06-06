ec7d76bfaf0afb60e387d8b46782fe32b6ff313d
Mario Viloria <211110@ids.upchiapas.edu.mx>
2024-06-05 23:01:32 -0600

    Initial commit

---
+++ test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const express = require('express');
+
+const app =express();
+app.get('api/vi/welcome', (req,res) => {
+    res.status(200).send('hello, world');
+});
+
+describe('GET /api/v1/welcome', () => {
+    it('respond with hello world', (done) => {
+        request(app)
+        .get('/api/v1/welcome')
+        .expect('hello world', done);
+    });
+});
