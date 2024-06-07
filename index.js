16d3f054d7817238ef7ca88d8cf641c5a327b59f
Mario Viloria <211110@ids.upchiapas.edu.mx>
2024-06-07 10:03:41 -0600

    debugger{jenkins pipeline try9}

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('hello, world');
+    res.send('hello, world!!!');
 });
 
 app.listen(PORT, () => {
--- test.js
+++ test.js
@@ -3,13 +3,13 @@ const express = require('express');
 
 const app =express();
 app.get('/api/v1/welcome', (req,res) => {
-    res.status(200).send('hello, world');
+    res.status(200).send('hello, world!!!');
 });
 
 describe('GET /api/v1/welcome', () => {
     it('respond with hello world', (done) => {
         request(app)
-        .get('/api/v1/welcome')
+        .get('/api/v1/welcome!!!')
         .expect('hello, world', done);
     });
 });
